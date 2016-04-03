8ba50e274dfa846227e617819d62af4165f8111c
Ripper <ricardo.pcan@gmail.com>
2016-04-02 20:21:42 -0600

    Add join table for entrepreneurs

--- src/db/migrate/20160327061411_create_photos.rb
+++ src/db/migrate/20160327061411_create_photos.rb
@@ -1,6 +1,14 @@
 class CreatePhotos < ActiveRecord::Migration
-  def change
+  def up
     create_table :photos do |t|
+      t.string name, null: true
+      t. string path, null:false
+      t.timestamps
     end
+    add_foreign_key :photos, :projects
   end
+
+  def down
+    remove_foreign_key :photos, :projects
+    drop_table :photos
 end
--- src/db/migrate/20160327061426_create_files.rb
+++ src/db/migrate/20160327061426_create_files.rb
@@ -1,6 +1,16 @@
 class CreateFiles < ActiveRecord::Migration
-  def change
+  def up
     create_table :files do |t|
+      t.string name, null: false
+      t.string path, null: false
+      t.timestamps
     end
+
+    add_foreign_key :files, :projects
+  end
+
+  def down
+    remove_foreign_key :files, :projects
+    drop_table :files
   end
 end
