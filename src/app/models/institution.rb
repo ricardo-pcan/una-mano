6e9b2d8ca26f2afbe20469fadd93be35e9dbd5c3
Ripper <ricardo.pcan@gmail.com>
2016-04-06 00:07:00 -0500

    Modify institution model

--- src/app/models/institution.rb
+++ src/app/models/institution.rb
@@ -1,2 +1,5 @@
 class Institution < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { minimum: 4, maximum: 100 }
+  validates :description, length: { maximum: 500 }
 end
