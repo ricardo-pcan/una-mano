c0bbe6003988ed007a307f8dabcc3a2540b30f43
Ripper <ricardo.pcan@gmail.com>
2016-05-07 13:22:11 -0500

    Fix hound issues

--- src/spec/models/institution_spec.rb
+++ src/spec/models/institution_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 require "institution"
 
 RSpec.describe Institution, type: :model do
@@ -25,70 +25,68 @@ RSpec.describe Institution, type: :model do
   end
 
   it "could has description" do
-    institution = build( :institution, :description_fill )
+    institution = build(:institution, :description_fill)
     institution.should be_valid
   end
 
   it "could not has description greater than 500 chars" do
-    institution = build( :institution, :description_over_500_char )
+    institution = build(:institution, :description_over_500_char)
     institution.should_not be_valid
   end
 
   it "could has description less than 500 chars" do
-    institution = build( :institution, :description_less_500_chars )
+    institution = build(:institution, :description_less_500_chars)
     institution.should be_valid
   end
 
   it "must be admin" do
-    institution = build( :institution, :with_admin )
+    institution = build(:institution, :with_admin)
     institution.should be_valid
   end
 
   it "could not hasnt admin" do
-    institution = build( :institution, :without_admin )
+    institution = build(:institution, :without_admin)
     institution.should_not be_valid
   end
 
   it "must has alias" do
-    institution = build( :institution, :with_alias )
+    institution = build(:institution, :with_alias)
     institution.should be_valid
   end
 
   it "couldn't has alias nil" do
-    institution = build( :institution, :without_alias )
+    institution = build(:institution, :without_alias)
     institution.should_not be_valid
   end
 
   it "couldn't has alias greater than 20 chars" do
-    institution = build( :institution, :alias_greater_than_20_char )
+    institution = build(:institution, :alias_greater_than_20_char)
     institution.should_not be_valid
   end
 
   it "could has alias less than 20 chars" do
-    institution = build( :institution, :alias_less_than_20_char )
+    institution = build(:institution, :alias_less_than_20_char)
     institution.should be_valid
   end
 
   it "could has image profile" do
-    institution = build( :institution, :with_image_profile )
+    institution = build(:institution, :with_image_profile)
     institution.should be_valid
   end
 
   it "could has not image profile" do
-    institution = build( :institution, :without_image_profile )
+    institution = build(:institution, :without_image_profile)
     institution.should be_valid
   end
 
   it "could has not image profile path less than 10 chars" do
-    institution = build( :institution, :with_image_profile_path_less_10 )
+    institution = build(:institution, :with_image_profile_path_less_10)
     institution.should_not be_valid
   end
 
   it "could create" do
-    institution = create( :institution )
+    institution = create(:institution)
     institution.should be_valid
   end
-
-  it { should belong_to(:admin) }
 
 end
--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -58,7 +58,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   #
-  
+
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
